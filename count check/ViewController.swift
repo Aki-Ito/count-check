693e9bf2f37c9da1e3742740f7d6665f39532b8a
Akitaka Ito <a2ki0ta0ka0@icloud.com>
2020-09-14 14:13:09 +0900

    Initial Commit

---
+++ count check/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  count check
+//
+//  Created by 伊藤明孝 on 2020/09/14.
+//  Copyright © 2020 com.litech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
