e21ce25e1da9814ea12cef8b99700559756da980
Akitaka Ito <a2ki0ta0ka0@icloud.com>
2020-09-14 14:31:28 +0900

    None

--- count check/ViewController.swift
+++ count check/ViewController.swift
@@ -9,12 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var number: Int = 0
+    @IBOutlet var Label: UILabel!
+  
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func plus(){
+        number=number+1
+        Label.text=String(number)
+    }
+    @IBAction func minus(){
+    number=number-1
+    Label.text=String(number)
+    }
+    @IBAction func clear(){
+    number=0
+    Label.text=String(number)
+    }
 }
 
